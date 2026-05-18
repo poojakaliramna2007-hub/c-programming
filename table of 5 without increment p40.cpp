8b7de01e564209d9d988047bb53b2c5279eb3adf
poojakaliramna2007-hub <poojakaliramna2007@gmail.com>
2026-05-18 07:00:20 -0800

    Add files via upload

---
+++ table of 5 without increment p40.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+   {
+   	int a;
+   	for(a=5;a<=50;)
+   	  {
+   	  	printf("\n%d",a);
+   	  	a=a+5;
+   	  	
+		 }
+	return 0;                                  	 
+   }

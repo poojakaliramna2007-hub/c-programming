09925cf406446cffcec5f1b8ce41dbe49858dfa8
poojakaliramna2007-hub <poojakaliramna2007@gmail.com>
2026-05-18 07:09:23 -0800

    Add files via upload

---
+++ prime number exit for loop.cpp
@@ -0,0 +1,18 @@
+#include<process.h>
+#include<stdio.h>
+int main()
+  {
+  	int a,b,no;
+  	printf("enter your number");
+  	scanf("%d",&no);
+  	for(a=2;a<no;a++)
+  	   {
+  	   	if(no%2==0)
+  	   	  {
+  	   	  	printf("composite");
+  	   	  	exit(0);
+			   }}
+		printf("prime");
+		return 0;	   
+		 }
+  

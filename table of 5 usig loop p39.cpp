298a32446d89ca2f4b50c0e883eb3c731d9ec7ad
poojakaliramna2007-hub <poojakaliramna2007@gmail.com>
2026-05-18 06:59:50 -0800

    Add files via upload

---
+++ table of 5 usig loop p39.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+   {
+   	int a=5;
+   	 while(a<=50)
+		 {
+		 	printf("\n%d",a);
+		 	a+=5;
+											}
+			return 0;								                              	 
+   }

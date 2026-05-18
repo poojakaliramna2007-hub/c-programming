21c1fc9b918cc6c3d3a4eba37ad8b6d32f984e7a
poojakaliramna2007-hub <poojakaliramna2007@gmail.com>
2026-05-18 07:26:12 -0800

    Add files via upload

---
+++ ch is vowel or consonent using operator 30.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+   {
+   	char ch;
+   	printf("enter character");
+   	scanf("%c",&ch);
+   	if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+   	printf("character is vowel");
+   	else
+   	printf("character is consonent");
+   	return 0;
+   }

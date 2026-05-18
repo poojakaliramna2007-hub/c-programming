7eb9ddac54589570c3d09073b929c087f2af5e58
poojakaliramna2007-hub <poojakaliramna2007@gmail.com>
2026-05-18 07:05:31 -0800

    Add files via upload

---
+++ pyramid.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+  {
+  	int a,b,c;
+  	for(a=1;a<=4;a++)
+  	  {
+  	  	for(c=3;c>=a;c--)
+  	  	   {
+  	  	   	printf(" ");
+			   }
+		for(b=1;b<=a;b++)
+		   {
+		   	printf(" %d",b);
+				  }
+				  printf("\n");	   
+		}
+	return 0;	
+  }

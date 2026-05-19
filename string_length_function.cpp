86c9345baab4a8075b9e3ddd51d9d33e7fe337d5
poojakaliramna2007-hub <poojakaliramna2007@gmail.com>
2026-05-18 23:27:47 -0800

    Add files via upload

---
+++ string_length_function.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+    {
+    	char ar[10];
+    	int length;
+    	printf("enter yuor string");
+    	gets(ar);
+    	length=strlen(ar);
+    	printf("%d",length);
+    	return 0;
+	}

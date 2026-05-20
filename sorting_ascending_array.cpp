2e99ea6a6c2b4e54684559b0db4db5159e1eeb08
poojakaliramna2007-hub <poojakaliramna2007@gmail.com>
2026-05-19 23:42:06 -0800

    Add files via upload

---
+++ sorting_ascending_array.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()                   //sorting in ascending order
+   {
+   	int ar[5],i,j,x;
+   	for(i=0;i<=4;i++)
+   	   {
+   	   	printf("enter your number");
+   	   	scanf("%d",&ar[i]);
+		  }
+	for(i=0;i<=4;i++)
+	  {
+	  	for(j=i+1;j<=4;j++)
+	  	   {
+	  	   	if(ar[i]>=ar[j])
+	  	   	{
+			x=ar[i];
+	  	   	ar[i]=ar[j];
+	  	   	ar[j]=x;}
+			 }
+			}	  
+	for(i=0;i<=4;i++)
+	   {
+	   	printf("%d ",ar[i]);
+			 }	  
+	return 0;		 
+   }

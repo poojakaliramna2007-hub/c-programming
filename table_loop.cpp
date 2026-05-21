6dd2e3465841f385a76e8c9fce38ba147f38e037
poojakaliramna2007-hub <poojakaliramna2007@gmail.com>
2026-05-21 10:01:01 -0800

    Add files via upload

---
+++ table_loop.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+   {
+   	int c,no,a;
+   	printf("enter your number");
+   	scanf("%d",&no);
+   	for(c=1;c<=10;c++)
+   	  {
+   	  	a=c*no;
+   	  	printf("%d ",a);
+		 }
+	return 0;	 
+   }

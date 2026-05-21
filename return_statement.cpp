df98534519d2a2840cd09f563d2da4c92a627ffe
poojakaliramna2007-hub <poojakaliramna2007@gmail.com>
2026-05-21 00:35:22 -0800

    Add files via upload

---
+++ return_statement.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int sum(int a,int b);
+int sum(int a,int b)
+   {
+   	return a+b;
+   }
+int main()
+    {
+    	int x=5,y=8;
+    	printf("%d",sum(x,y));
+    	return 0;
+	   }   

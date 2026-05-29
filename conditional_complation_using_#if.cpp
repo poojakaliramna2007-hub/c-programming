b242d345135ad4b5787649490a2ef14304318a18
poojakaliramna2007-hub <poojakaliramna2007@gmail.com>
2026-05-29 04:18:10 -0800

    Add files via upload

---
+++ conditional_complation_using_#if.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#define start int main(){
+#define NUM -5
+#define end return 0;}
+start 
+#if(NUM>0)
+printf("positive number");
+#else
+printf("negative number");
+#endif
+end	

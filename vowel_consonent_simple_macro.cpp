ec1146f22945ce227f412c9f71e85974d6269cb8
poojakaliramna2007-hub <poojakaliramna2007@gmail.com>
2026-05-29 04:17:44 -0800

    Add files via upload

---
+++ vowel_consonent_simple_macro.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#define start int main(){
+#define OR ||
+#define end return 0;}
+start
+char ch;
+printf("enter your character");
+scanf(" %c",&ch);
+if(ch=='a' OR ch=='e' OR ch=='i' OR ch=='o' OR ch=='u')
+printf("character is vowel");
+else
+printf("consonent");
+end
